6a45f6eae030a5f45627def366a997f7e7382d41
Dirk Eddelbuettel <edd@debian.org>
2025-01-09 21:34:34 -0600

    Now returns a table a proof of concept

--- src/mlpack_extension.cpp
+++ src/mlpack_extension.cpp
@@ -45,6 +45,61 @@ inline void MlpackMlpackVersionScalarFun(DataChunk &args, ExpressionState &state
         });
 }
 
+// Table code below
+
+struct MlpackTableData : public GlobalTableFunctionState, public TableFunctionData {
+    bool data_returned = false;  // Add this flag
+    vector<Value> col1;
+    vector<Value> col2;
+    vector<Value> col3;
+    vector<Value> col4;
+
+    MlpackTableData() {}
+
+    idx_t MaxThreads() const override {
+        return 1;
+    }
+};
+
+static unique_ptr<GlobalTableFunctionState> MlpackTableInit(ClientContext &context, TableFunctionInitInput &input) {
+    return make_uniq<MlpackTableData>();
+}
+
+static unique_ptr<FunctionData> MlpackTableBind(ClientContext &context, TableFunctionBindInput &input,
+												vector<LogicalType> &return_types, vector<string> &names) {
+
+    names = { "col_1", "col_2", "col_3", "col_4" };
+    return_types = { LogicalType::SMALLINT, LogicalType::INTEGER, LogicalType::BIGINT, LogicalType::DOUBLE };
+
+    return make_uniq<MlpackTableData>();
+}
+
+static void MlpackTableFunction(ClientContext &context, TableFunctionInput &data_p, DataChunk &output) {
+
+	auto &data = data_p.global_state->Cast<MlpackTableData>();
+
+	// if we have been called, return nothing
+    if (data.data_returned) {
+        output.SetCardinality(0);
+        return;
+    }
+
+	idx_t chunk_size = 3; // arbitrary
+
+    output.SetCardinality(chunk_size);
+
+	int32_t val = 0;
+
+    for (idx_t i = 0; i < chunk_size; i++) {
+        output.data[0].SetValue(i, val++);
+        output.data[1].SetValue(i, val++);
+        output.data[2].SetValue(i, val++);
+        output.data[3].SetValue(i, val++);
+    }
+
+	// mark that we have been called
+	data.data_returned = true;
+}
 
 static void LoadInternal(DatabaseInstance &instance) {
     // Register a scalar function
@@ -53,14 +108,18 @@ static void LoadInternal(DatabaseInstance &instance) {
 
     // Register another scalar function
     auto mlpack_openssl_version_scalar_function = ScalarFunction("mlpack_openssl_version", {LogicalType::VARCHAR},
-                                                LogicalType::VARCHAR, MlpackOpenSSLVersionScalarFun);
+																 LogicalType::VARCHAR, MlpackOpenSSLVersionScalarFun);
     ExtensionUtil::RegisterFunction(instance, mlpack_openssl_version_scalar_function);
 
     // Register another scalar function
     auto mlpack_mlpack_version_scalar_function = ScalarFunction("mlpack_mlpack_version", {LogicalType::VARCHAR},
-                                                LogicalType::VARCHAR, MlpackMlpackVersionScalarFun);
+																LogicalType::VARCHAR, MlpackMlpackVersionScalarFun);
     ExtensionUtil::RegisterFunction(instance, mlpack_mlpack_version_scalar_function);
 
+	// Register sample table function
+	auto mlpack_sample_table_function = TableFunction("mlpack_table", {}, MlpackTableFunction, MlpackTableBind);
+	mlpack_sample_table_function.init_global = MlpackTableInit;  // Add this line
+	ExtensionUtil::RegisterFunction(instance, mlpack_sample_table_function);
 }
 
 void MlpackExtension::Load(DuckDB &db) {
